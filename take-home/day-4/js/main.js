fbd641c9b7a8b2d1fa5d74681116344def19d02a
Ricky Eckert <ricky_eckert@yahoo.com>
2022-07-12 18:54:26 -0700

    -re

---
+++ take-home/day-4/js/main.js
@@ -0,0 +1,22 @@
+const firstNumber = prompt('First Number');
+const secondNumber = prompt('Second Number');
+const firstInt = parseInt(firstNumber);
+const secondInt = parseInt(secondNumber);
+console.log(firstInt);
+if (firstInt > secondInt) {
+  alert(`${firstInt} is bigger than ${secondInt}`);
+}
+if (firstInt < secondInt) {
+  alert(`${secondInt} is bigger than ${firstInt}`);
+}
+if (firstInt == secondInt)  {
+  alert(`${firstInt} and ${secondInt} are equal`);
+}
+if (!firstInt) {
+    alert('Please enter digits only.');
+    firstNumber = prompt('First Number');
+  }
+  if (!secondInt) {
+    alert('Please enter digits only.');
+    secondNumber = prompt('Second Number');
+  }
